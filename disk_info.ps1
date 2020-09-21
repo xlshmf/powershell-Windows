ac623cc52acbdf94cc2d917e4129e8fabeeaad10
Leshem <xlshmf@163.com>
2020-09-21 09:43:59 +0800

    Add files via upload

---
+++ disk_info.ps1
@@ -0,0 +1,7 @@
+﻿Get-WmiObject -Class Win32_LogicalDisk -ComputerName localhost `
+-Filter "drivetype=3"|
+Sort-Object -Property DeviceID | 
+Format-Table -Property DeviceID, 
+@{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
+@{label='Size(GB)';expression={$_.Size / 1GB -as [int]}},
+@{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
